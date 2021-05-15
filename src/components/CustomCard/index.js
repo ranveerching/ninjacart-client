a10bcfa6fd886376cd964634218e112ed4b9c69a
Pankaj Sood <pankajhoney1991@gmail.com>
2021-05-15 18:22:01 +0530

    Replaced images with iframes.

--- src/components/CustomCard/index.js
+++ src/components/CustomCard/index.js
@@ -34,27 +34,11 @@ const CustomCard = props => {
         bordered={bordered}
         size='small'
         className='responsive-grid-title shadow-lg custom-card'
+        title={<Title level={4} className='text-white text-left mt-2 mb-2' ellipsis>{item?.name ?? 'N/A'}</Title>}
         cover={
-          <>
-            <img
-              alt=""
-              src={item?.image}
-              className='position-absolute image-container'
-              onLoadStart={() => setLoading(true)}
-              onLoad={() => console.log('Load Success!')}
-              onError={() => console.log('Load Failed!!!')}
-            />
-            <div className='linear-gradient-container'>
-              <div className='rating-container'>
-                <div className='d-flex flex-row align-items-center'>
-                  <Rate allowHalf value={item?.rating} onChange={val => setRating(item?.id, val)} />
-                  <Text className='text-white pt-1 pl-2 font-italic rating-number-text'>
-                    {item?.rating}
-                  </Text>
-                </div>
-              </div>
-            </div>
-          </>
+          <div className="embed-responsive embed-responsive-16by9">
+            <iframe title={item.title} className="embed-responsive-item" src='https://angular.io/'></iframe>
+          </div>
         }
       >
         <Card.Meta
@@ -63,11 +47,10 @@ const CustomCard = props => {
               direction={direction}
               className='w-100'
             >
-              <Title level={4} className='text-danger text-left mt-1' ellipsis>{item?.name ?? 'N/A'}</Title>
               <a
                 href={`${item?.url}`}
                 target='blank'
-                className='link btn btn-danger w-100 mt-0 mb-2 font-weight-bold'>
+                className='link btn btn-danger w-100 mt-2 mb-2 font-weight-bold'>
                 Navigate
               </a>
             </Space>
--- src/utils/fakeData.js
+++ src/utils/fakeData.js
@@ -4,31 +4,31 @@ const fakeData = [{
   id: uuidv4(),
   name: 'Google',
   image: 'https://1.bp.blogspot.com/-gJSIqMr9Kjg/YJ1QkUtNpdI/AAAAAAAABhY/DsMXDOk2Ko0-mSjYtCocrzjpuHF40VcuQCLcBGAsYHQ/s0/google.png',
-  url: 'https://google.com/',
+  url: 'http://www.yahoo.com',
   rating: 0,
 }, {
   id: uuidv4(),
   name: 'Facebook',
   image: 'https://1.bp.blogspot.com/-x8kdGWF_8vU/YJ1R4cXB3_I/AAAAAAAABh8/VsBS4nonokEn-ZH1f40lBq9DXs7BoakVgCLcBGAsYHQ/s0/facebook.png',
-  url: 'https://facebook.com/',
+  url: 'https://www.facebook.com/',
   rating: 0,
 }, {
   id: uuidv4(),
   name: 'Apple',
   image: 'https://1.bp.blogspot.com/-dIfJwYxZN24/YJ1QkQCu55I/AAAAAAAABhc/1o7DiMlmbHY-IDbGCvMshRhhUnxDSuquACLcBGAsYHQ/s0/apple.png',
-  url: 'https://apple.com/',
+  url: 'https://www.apple.com/',
   rating: 0,
 }, {
   id: uuidv4(),
   name: 'Amazon',
   image: 'https://1.bp.blogspot.com/-PZqjsb-pGUY/YJ1QkaDoLGI/AAAAAAAABhg/A0ZuINLrOKcvKR78imbDH4BImqCJvHEIACLcBGAsYHQ/s0/amazon.png',
-  url: 'https://amazon.com/',
+  url: 'https://www.amazon.com/',
   rating: 0,
 }, {
   id: uuidv4(),
   name: 'Microsoft',
   image: 'https://1.bp.blogspot.com/-KLH7deH77Ek/YJ1QlTtPKzI/AAAAAAAABhk/VOHqbQhHUYIgDC_AbUsLE4HR_9EVvbuwgCLcBGAsYHQ/s0/microsoft.png',
-  url: 'https://microsoft.com/',
+  url: 'https://www.microsoft.com/',
   rating: 0,
 }];
 
